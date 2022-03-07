19fb6408e51f99beb125f3678e0043e5b81aa0f1
mjn81 <mjjnn81@gmail.com>
2022-03-07 23:12:14 +0330

    Project Complete

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,12 +1,53 @@
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import './About.scss'
+import { AppWrapper, MotionWrapper } from "../../wrapper";
+import { images } from "../../constants";
+import "./About.scss";
+import { urlFor, client } from "../../client";
 
 const About = () => {
-    return ( 
-        <div>
-            about     
-        </div>
-     );
-}
- 
-export default About;
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+		client.fetch(query).then((data) => setAbouts(data));
+	}, []);
+
+	return (
+		<>
+			<h2 className="head-text">
+				I know that
+				<span>Good Design</span>
+				<br />
+				means
+				<span>Good Business</span>
+			</h2>
+			<div className="app__profiles">
+				{abouts.map((about, index) => (
+					<motion.div
+						whileInView={{ opacity: 1 }}
+						whileHover={{ scale: 1.1 }}
+						transition={{ duration: 0.5, type: "tween" }}
+						className="app__profile-item"
+						key={about.title + index}
+					>
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
+						<h2 className="bold-text" style={{ marginTop: 20 }}>
+							{about.title}
+						</h2>
+						<h2 className="p-text" style={{ marginTop: 10 }}>
+							{about.description}
+						</h2>
+					</motion.div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default AppWrapper(
+	MotionWrapper(About, "app__about"),
+	"about",
+	"app__whitebg"
+);
--- src/container/Testimonials/Testimonials.jsx
+++ src/container/Testimonials/Testimonials.jsx
@@ -1,7 +1,77 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+
+import { AppWrapper, MotionWrapper } from "../../wrapper";
+import { urlFor, client } from "../../client";
+
 import "./Testimonials.scss";
 
 const Testimonials = () => {
-	return <div>test</div>;
+	const [brands, setBrands] = useState([]);
+	const [testimonials, setTestimonials] = useState([]);
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const handleClick = (index) => {
+		setCurrentIndex(index);
+	}
+
+	useEffect(() => {
+		const query = '*[_type == "testimonials"]';
+		const brandQuery = '*[_type == "brands"]';
+		client.fetch(query).then((data) => {
+			setTestimonials(data);
+		});
+		client.fetch(brandQuery).then((data) => {
+			setBrands(data);
+		});
+	}, []);
+
+	const crrtest = testimonials[currentIndex];
+
+	return (
+		<>
+			{testimonials.length && (
+				<>
+					<div className="app__testimonial-item app__flex">
+						<img src={urlFor(crrtest.imgurl)} alt="testimonial" />
+						<div className="app__testimonial-content">
+							<p className="p-text">{crrtest.feedback}</p>
+							<div>
+								<h4 className="text-bold">{crrtest.name}</h4>
+								<h5 className="p-text">{crrtest.company}</h5>
+							</div>
+						</div>
+					</div>
+					<div className="app__testimonial-btns app__flex">
+						<div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+							<HiChevronLeft />
+						</div>
+						<div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+							<HiChevronRight />
+						</div>
+					</div>
+				</>
+			)}
+
+			<div className="app__testimonial-brands app__flex">
+				{brands.map(brand => (
+					<motion.div
+						whileInView={{ opacity: [0, 1], }}
+						transition={{ duration: 0.5, type: 'tween' }}
+						key={brand._id}
+					>
+						<img src={urlFor(brand.imgUrl)} alt={brand.name}	 />
+					</motion.div>
+				))}
+
+			</div>
+		</>
+	);
 };
 
-export default Testimonials;
+export default AppWrapper(
+	MotionWrapper(Testimonials, "app__testimonial"),
+	"testimonial",
+	"app__primarybg"
+);
